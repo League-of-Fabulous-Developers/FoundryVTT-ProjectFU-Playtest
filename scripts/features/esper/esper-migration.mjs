8b10318f23b6a016d476911ae02d47d5d6aaefcb
Sören Jahns <jahns.simon@web.de>
2024-05-20 18:40:57 +0200

    Improve migration system

--- scripts/features/esper/esper-migration.mjs
+++ scripts/features/esper/esper-migration.mjs
@@ -1,38 +1,18 @@
 import {Migration} from "../../migration.mjs";
 import {PsychicGiftDataModel} from "./psychic-gift-data-model.mjs";
-import {MODULE} from "../../constants.mjs";
 import {SETTINGS} from "../../settings.mjs";
 
 export class EsperMigration extends Migration {
 
-    canRun() {
-        return "psychicGift" in this.systemFeatures && "psychicGift" in this.moduleFeatures
-    }
-
-    run() {
-        const items = game.items.search({
-            filters: [
-                {field: "type", value: "classFeature"},
-                {field: "system.featureType", value: this.moduleFeatures.psychicGift}
-            ]
-        });
-        game.actors.forEach(actor => {
-            actor.itemTypes.classFeature.forEach(classFeature => {
-                if (classFeature.system.featureType === this.moduleFeatures.psychicGift) {
-                    items.push(classFeature)
-                }
-            })
-        })
-
-
-        const migrations = items.map(item => item.update({"system.featureType": this.systemFeatures.psychicGift}, {noHook: true}));
-
-        migrations.push(game.settings.set(MODULE, SETTINGS.classes.esper, false))
-
-        return migrations;
-    }
-
     get affectedFeatures() {
-        return [PsychicGiftDataModel];
+        return [{
+            module: this.moduleFeatures.psychicGift,
+            system: this.systemFeatures.psychicGift,
+            implementation: PsychicGiftDataModel
+        }];
+    }
+
+    get setting(){
+        return SETTINGS.classes.esper;
     }
 }
--- scripts/features/pilot/pilot-migration.mjs
+++ scripts/features/pilot/pilot-migration.mjs
@@ -9,58 +9,53 @@ import {VEHICLE_FLAGS} from "./vehicle-manager.mjs";
 
 export class PilotMigration extends Migration {
 
-    static #features = ["vehicle", "armorModule", "weaponModule", "supportModule"];
-
-    canRun() {
-        return PilotMigration.#features
-            .every(key => key in this.systemFeatures && key in this.moduleFeatures)
-    }
-
-    run() {
-        const migrations = [];
-        for (let feature of PilotMigration.#features) {
-            const items = game.items.search({
-                filters: [
-                    {field: "type", value: "classFeature"},
-                    {field: "system.featureType", value: this.moduleFeatures[feature]}
-                ]
-            });
-            game.actors.forEach(actor => {
-                actor.itemTypes.classFeature.forEach(classFeature => {
-                    if (classFeature.system.featureType === this.moduleFeatures[feature]) {
-                        items.push(classFeature)
-                    }
-                })
-            })
-
-            migrations.push(...items.map(item => item.update({"system.featureType": this.systemFeatures[feature]}, {noHook: true})));
-        }
-
-        game.actors.filter(actor => actor.vehicleManager)
-            .forEach(actor => {
+    additionalMigrations() {
+        return game.actors.filter(actor => actor.vehicleManager)
+            .map(actor => {
                 const activeVehicle = actor.getFlag(MODULE, VEHICLE_FLAGS.activeVehicle);
                 const vehicleEmbarked = actor.getFlag(MODULE, VEHICLE_FLAGS.vehicleEmbarked);
                 const activeArmor = actor.getFlag(MODULE, VEHICLE_FLAGS.activeArmor);
                 const activeWeapons = actor.getFlag(MODULE, VEHICLE_FLAGS.activeWeapons);
                 const activeSupports = actor.getFlag(MODULE, VEHICLE_FLAGS.activeSupports);
 
-                migrations.push(actor.update({
+                return () => actor.update({
                     [`flags.${MODULE}.-=vehicle`]: null,
                     "system.vehicle.vehicle": activeVehicle,
                     "system.vehicle.embarked": vehicleEmbarked,
                     "system.vehicle.armor": activeArmor,
                     "system.vehicle.weapons": activeWeapons,
                     "system.vehicle.supports": activeSupports,
-                }))
+                })
             })
-
-        migrations.push(game.settings.set(MODULE, SETTINGS.classes.pilot, false))
-
-        return migrations;
-
     }
 
     get affectedFeatures() {
-        return [VehicleDataModel, ArmorModuleDataModel, WeaponModuleDataModel, SupportModuleDataModel];
+        return [
+            {
+                module: this.moduleFeatures.vehicle,
+                system: this.systemFeatures.vehicle,
+                implementation: VehicleDataModel
+            },
+            {
+                module: this.moduleFeatures.armorModule,
+                system: this.systemFeatures.armorModule,
+                implementation: ArmorModuleDataModel
+            },
+            {
+                module: this.moduleFeatures.weaponModule,
+                system: this.systemFeatures.weaponModule,
+                implementation: WeaponModuleDataModel
+            },
+            {
+                module: this.moduleFeatures.supportModule,
+                system: this.systemFeatures.supportModule,
+                implementation: SupportModuleDataModel
+            }
+        ]
+    }
+
+
+    get setting() {
+        return SETTINGS.classes.pilot
     }
 }
